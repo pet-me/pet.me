1d12b4d5530e110e08788eb1f7bd70eba4e676a9
erinbell1990 <erinbell1990@gmail.com>
2015-09-28 15:20:10 -0700

    Update services.js

--- Petme/www/js/services.js
+++ Petme/www/js/services.js
@@ -1,28 +1,52 @@
 angular.module('starter.services', [])
 
-    .service('LoginService', function ($http, $q) {
-        return {
-            loginUser: function (name, pw) {
-                var deferred = $q.defer();
-                var promise = deferred.promise;
+.service('LoginService', function($q) {
+    return {
+        loginUser: function(name, pw) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
 
-                if (name == deferred.loginUser && pw == deferred.pw) {
-                    deferred.resolve('Welcome ' + name + '!');
-                } else {
-                    deferred.reject('Wrong credentials.');
-                }
-                promise.success = function (fn) {
-                    promise.then(fn);
-                    return promise;
-                }
-                promise.error = function (fn) {
-                    promise.then(null, fn);
-                    return promise;
-                }
+            if (name == 'user' && pw == 'secret') {
+                deferred.resolve('Welcome ' + name + '!');
+            } else {
+                deferred.reject('Wrong credentials.');
+            }
+            promise.success = function(fn) {
+                promise.then(fn);
                 return promise;
             }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
         }
-    })
+    }
+})
+.service('RegisterService', function($q) {
+    return {
+        registerUser: function(reg_user, reg_pass) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+
+            if (reg_user == 'user' && reg_pass == 'secret') {
+                deferred.resolve('Welcome ' + reg_user + '!');
+            } else {
+                deferred.reject('Wrong credentials.');
+            }
+            promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+        }
+    }
+})
+
 
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
@@ -37,10 +61,10 @@ angular.module('starter.services', [])
     id: 1,
     name: 'Max Lynx',
     lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    face: 'https://pbs.twimg.com/profile_images/479740132258361344/KaYdH9hE.jpeg'
   }, {
     id: 2,
-    name: 'Andrew Jostlin',
+    name: 'Andrew Jostlen',
     lastText: 'Did you get the ice cream?',
     face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
   }, {
@@ -70,5 +94,52 @@ angular.module('starter.services', [])
       }
       return null;
     }
-  };
+  }
+})
+
+/**
+ * A simple example service that returns some data.
+ */
+.factory('Friends', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  // Some fake testing data
+  var friends = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    notes: 'Enjoys drawing things',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    notes: 'Odd obsession with everything',
+    face: 'https://pbs.twimg.com/profile_images/479740132258361344/KaYdH9hE.jpeg'
+  }, {
+    id: 2,
+    name: 'Andrew Jostlen',
+    notes: 'Wears a sweet leather Jacket. I\'m a bit jealous',
+    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
+  }, {
+    id: 3,
+    name: 'Adam Bradleyson',
+    notes: 'I think he needs to buy a boat',
+    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+  }, {
+    id: 4,
+    name: 'Perry Governor',
+    notes: 'Just the nicest guy',
+    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+  }];
+
+
+  return {
+    all: function() {
+      return friends;
+    },
+    get: function(friendId) {
+      // Simple index lookup
+      return friends[friendId];
+    }
+  }
 });
