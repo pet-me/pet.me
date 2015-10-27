14d14ab2ca5ba855d32f04b8834833ad38912952
Snakebee <fjf.colourless@gmail.com>
2015-10-27 11:43:05 -0700

    init

--- Petme/www/js/controllers.js
+++ Petme/www/js/controllers.js
@@ -1,38 +1,26 @@
 angular.module('starter.controllers', [])
 
-.controller('LoginCtrl', ['$scope', '$state', 'LoginService',
-                             function ($scope, $state, LoginService) {
-                           	$scope.data = {};
-                           	
-                           	$scope.login = function() {
-                               LoginService.loginUser($scope.data.reg_user, $scope.data.reg_pass).then(function(data)
-                               {
-                           		console.log('returned JSON STRING: ' + data);
-                               
-                               if (!(data.username == 'null'))
-                            	   {
-                            	   		window.localStorage.setItem('token', data.username);
-                            	   		console.log('token get ' + window.localStorage['token']);
-                            	   		
-                            	   		$state.go('tab.dash');
-                            	   }
-                               
-                               else
-                            	   {
-                            	   		window.alert('Invalid login username/password');
-                            	   }
-                           		
-                               });
-                           	} 
-                           }
-                            ])
+.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
+    $scope.data = {};
+
+    $scope.login = function() {
+        LoginService.loginUser($scope.data.reg_user, $scope.data.reg_pass).success(function(data) {
+            $state.go('tab.dash');
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!'
+            });
+        });
+    }
+})
 
 .controller('RegisterCtrl', ['$scope', 'RegisterService',
   function ($scope, RegisterService) {
 	$scope.data = {};
 	
 	$scope.register = function() {
-    RegisterService.registerUser($scope.data.reg_user, $scope.data.reg_pass).then(function(data)
+    RegisterService.registerUser($scope.data.reg_user, $scope.data.reg_pass, $scope.data.email).then(function(data)
     {
 		console.log('returned JSON STRING: ' + data);
      //TODO, parse JSON data and change webpage
@@ -41,16 +29,31 @@ angular.module('starter.controllers', [])
 	} 
 }
  ])
-
-
-
-.controller('DashCtrl', function($scope) {})
-
+.controller('WelcomeCtrl', function($scope) {})
+/*
+.controller('QuestionnaireCtrl', function($scope,$stateParams,QuestionnaireService) {
+    $scope.data = {};
+	
+	$scope.questionnaire = function() {
+    QuestionnaireService.getUser($scope.data.reg_user, $scope.data.reg_pass).then(function(data)
+    {
+	$scope.reg_username=QuestionnaireService.get($stateParams.reg_user);
+   
+    });
+	}
+})
+*/
+.controller('QuestionnaireCtrl', function($scope) {})
+.controller('CreateProfileCtrl', function($scope) {})
+.controller('Profile', function($scope) {})
+.controller('PetCtrl', function($scope) {})
+.controller('PassCtrl', function($scope) {})
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
   }
+  
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
@@ -59,6 +62,10 @@ angular.module('starter.controllers', [])
 
 .controller('FriendsCtrl', function($scope, Friends) {
   $scope.friends = Friends.all();
+  $scope.warn = function (message) {
+      alert(message);
+    };
+  // use this is picture viewer page slide to look at pics and set active slide to chosen picture $scope.myActiveSlide = 1;
 })
 
 .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
